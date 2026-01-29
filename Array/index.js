e2caadcf2211c82c0b8db4cb0318cbb2f9cac1eb
SidharthSinghShrinet <tushar15463@gmail.com>
2026-01-29 21:49:06 +0530

    Array Practice 29/01/2026:- 4 Questions

--- Array/index.js
+++ Array/index.js
@@ -176,3 +176,72 @@ function occurences(arr) {
 
 occurences([10, 1, 5, 1, 10, 6, 1]);
 */
+
+//! Date:- 29/01/2026
+
+//^ Question 1(Implement the Linear Search)
+/*
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch([10, 20, 30, 40, 50], 30));
+*/
+
+//^ Question 2(Implement the Binary Search)
+/*
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = Math.trunc((start + end) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+    } else if (arr[mid] > target) {
+      end = mid - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch([10, 20, 30, 40, 50, 60, 70], 70));
+*/
+
+//^ Question 3(Implement the Bubble Sort)
+/*
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  console.log(arr);
+}
+
+bubbleSort([50, 70, 40, 30, 20, 10]);
+*/
+
+//^ Question 4(Implement the Selection Sort)
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let index = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[index] > arr[j]) {
+        index = j;
+      }
+    }
+    [arr[i], arr[index]] = [arr[index], arr[i]];
+  }
+  console.log(arr);
+}
+
+selectionSort([50, 70, 40, 30, 20, 10]);
